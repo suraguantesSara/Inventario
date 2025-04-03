7e17cbd399a9312dbed1c61622baa19190ad8dd9
suraguantesSara <aprendizsuraguantes@gmail.com>
2025-04-03 10:43:56 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("sql109.infinityfree.com", "if0_38651999", "TU_CONTRASEÑA", "if0_38651999_inventario_db");
+$conexion = new mysqli("sql109.infinityfree.com", "if0_38651999", "suraguantes2025", "if0_38651999_inventario_db");
 
 if ($conexion->connect_error) {
     die("❌ Error de conexión: " . $conexion->connect_error);
