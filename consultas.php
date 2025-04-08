81a746a2384f337e9221df9c4daae8a5614f91b1
suraguantesSara <aprendizsuraguantes@gmail.com>
2025-04-08 09:43:56 -0500

    Update consultas.php

--- consultas.php
+++ consultas.php
@@ -3,7 +3,7 @@ header("Content-Type: application/json");
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$url = "https://script.google.com/macros/s/AKfycbzRO7aDjWQ76e23GZy8mF0EH912TJDS3h2WejCUk3e3ownvXMrnZjPpjZmLhBlylb20/exec"; // Tu URL de Apps Script
+$url = "https://script.google.com/macros/s/AKfycbzRO7aDjWQ76e23GZy8mF0EH912TJDS3h2WejCUk3e3ownvXMrnZjPpjZmLhBlylb20/exec"; // URL de Apps Script
 
 $response = file_get_contents($url);
 
