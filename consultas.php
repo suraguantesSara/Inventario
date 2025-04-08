ef6395786bb334815910ac79888c662bc033f290
suraguantesSara <aprendizsuraguantes@gmail.com>
2025-04-08 09:15:32 -0500

    Add files via upload

---
+++ consultas.php
@@ -0,0 +1,11 @@
+<?php
+header("Content-Type: application/json"); 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$url = "https://script.google.com/macros/s/AKfycbzRO7aDjWQ76e23GZy8mF0EH912TJDS3h2WejCUk3e3ownvXMrnZjPpjZmLhBlylb20/exec"; // Tu URL de Apps Script
+
+$response = file_get_contents($url);
+
+echo $response; // Devuelve los datos en formato JSON
+?>
